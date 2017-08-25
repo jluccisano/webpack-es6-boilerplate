4f2a5edd8c425d7b54a65632204e162349bfa801
jls <j.luccisano@evs.com>
2017-08-25 15:35:20 +0200

    adding sass management

--- conf/webpack.common.js
+++ conf/webpack.common.js
@@ -2,53 +2,77 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "name.[contenthash].css",
+    disable: process.env.NODE_ENV !== "production"
+});
 
 module.exports = {
 
-  context: path.join(process.cwd(), 'src'), //the home directory for webpack
+    context: path.join(process.cwd(), 'src'), //the home directory for webpack
 
-  devtool: 'source-map', // enhance debugging by adding meta info for the browser devtools
+    devtool: 'source-map', // enhance debugging by adding meta info for the browser devtools
 
-  entry: {
-    app: './index.js'
-  },
+    entry: {
+        app: './index.js'
+    },
 
-  output: {
-    path: path.join(process.cwd(), 'dist'),
-    filename: '[name].[hash].js',
-    publicPath: '/',
-    sourceMapFilename: '[name].map'
-  },
+    output: {
+        path: path.join(process.cwd(), 'dist'),
+        filename: '[name].[hash].js',
+        publicPath: '/',
+        sourceMapFilename: '[name].map'
+    },
 
-  resolve: {
-    extensions: ['.js'],  // extensions that are used
-    modules: [path.join(process.cwd(), 'src'), 'node_modules'] // directories where to look for modules
-  },
+    resolve: {
+        extensions: ['.js'],  // extensions that are used
+        modules: [path.join(process.cwd(), 'src'), 'node_modules'] // directories where to look for modules
+    },
 
-  module: {
-    rules: [{
-      enforce: "pre", //to check source files, not modified by other loaders (like babel-loader)
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: "eslint-loader"
-    }, {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['env']
-        }
-      }
-    }]
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist'], {root: process.cwd()}),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "vendor"
-    }),
-    new HtmlWebpackPlugin({
-      template: 'index.html'
-    })
-  ]
+    module: {
+        rules: [{
+            enforce: "pre", //to check source files, not modified by other loaders (like babel-loader)
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "eslint-loader"
+        }, {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            }
+        }, {
+            test: /\.scss$/,
+            use: extractSass.extract({
+                use: [{
+                    loader: "css-loader",
+                    options: {
+                        sourceMap: true
+                    }
+                }, {
+                    loader: "sass-loader",
+                    options: {
+                        sourceMap: true
+                    }
+                }],
+                // use style-loader in development
+                fallback: "style-loader"
+            })
+        }]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist'], {root: process.cwd()}),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "vendor"
+        }),
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        extractSass
+    ]
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import sayHello from './hello';
+import './index.scss';
 
 document.getElementById('root').innerHTML = sayHello();
-
-
